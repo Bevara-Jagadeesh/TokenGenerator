0cf2e4da794fe57617787b032b927a4a579b18ae
jagadeesh.bevara <jagadeesh.bevara>
2023-02-02 01:35:00 +0530

    changes related to auth code

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ import {
 } from 'react-router-dom';
 
 function App() {
-  // const GetTokenCode = () => {
-  //   console.log(FHIR);
-  //   var code = FHIR.oauth2.authorize({
-  //                clientId: "3dce1300-ce75-4fd3-81a4-561618f4aa6f",
-  //                scope: "patient/Patient.read patient/Observation.read launch online_access openid profile",
-  //                iss:"https://fhir-ehr-code.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d",
-  //  // <!--              patientId:"12724065",-->
-  //                redirectUri: "http://10.10.13.14:8000",
-  //                launch:"a7b726f7-ea3c-4ad7-bef4-4eab4b3b0857",
-  //                response_type:"code"
-  //              });
-  //     console.log(" code   ", code);     
-  // }
+  const GetTokenCode = () => {
+    console.log(FHIR);
+    var code = FHIR.oauth2.authorize({
+                 clientId: "3dce1300-ce75-4fd3-81a4-561618f4aa6f",
+                 scope: "patient/Patient.read patient/Observation.read launch online_access openid profile",
+                 iss:"https://fhir-ehr-code.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d",
+   // <!--              patientId:"12724065",-->
+                 redirectUri: "http://10.10.13.14:8000",
+                 launch:"a7b726f7-ea3c-4ad7-bef4-4eab4b3b0857",
+                 response_type:"code"
+               });
+      console.log(" code   ", code);     
+  }
   return (
     <Router>
     <div className="App">
--- src/home.js
+++ src/home.js
@@ -1,8 +1,64 @@
 import React from 'react';
+import FHIR from 'fhirclient';
 
 function Home () {
-	return <div>
-		<h2>Hi</h2>		
-	</div>
+  
+    
+    FHIR.oauth2.ready().then(function(client) {
+
+        // Render the current patient (or any error)
+        client.patient.read().then(
+            function(pt) {
+                document.getElementById("patient").innerText = JSON.stringify(pt, null, 4);
+            },
+            function(error) {
+                document.getElementById("patient").innerText = error.stack;
+            }
+        )
+
+        // Get MedicationRequests for the selected patient
+        // client.request("/Observation?patient=" + client.patient.id, {
+        //     resolveReferences= [ "valueQuantity" ],
+        //     graph= true
+        // })
+
+        // // Reject if no MedicationRequests are found
+        // .then(function(data) {
+        //     if(!data.entry || !data.entry.length) {
+        //         throw new Error("No observation found for the selected patient");
+        //     }
+        //     return data.entry;
+        // })
+
+
+        // // Render the current patient's medications (or any error)
+        // .then(
+        //     function(meds) {
+        //         document.getElementById("meds").innerText = JSON.stringify(meds, null, 4);
+        //     },
+        //     function(error) {
+        //         document.getElementById("meds").innerText = error.stack;
+        //     }
+        // );
+
+    }).catch(console.error);
+
+	return <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Example SMART App</title>
+        <script src="https://cdn.jsdelivr.net/npm/fhirclient/build/fhir-client.js"></script>
+        
+    </head>
+    <body>
+        <h4>Current Patient</h4>
+
+        <div id="patient">Loading...</div>
+        <br/>
+        <h4>Observations</h4>
+        <div id="meds">Loading...</div>
+       
+    </body>
+</html>
 }
 export default Home;
